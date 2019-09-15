dc4bc27fdd488370175ca96104badd2ffdcd9bd7
jacm80 <juan.canepa>
2019-09-15 18:21:22 -0300

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t jacm80/multi-client:latest -t jacm80/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t jacm80/multi-server:latest -t jacm80/multi-server:$SHA ./server/Dockerfile ./server
+docker build .t jacm80/multi-worker:latest -t jacm80/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push jacm80/multi-client:latest
+docker push jacm80/multi-server:latest
+docker push jacm80/multi-worker:latest
+
+docker push jacm80/multi-client:$SHA
+docker push jacm80/multi-server:$SHA
+docker push jacm80/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=jacm80/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jacm80/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jacm80/multi-worker:$SHA
